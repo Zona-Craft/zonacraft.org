b0860e816c8c5bfc16eb3ce4526b840358ecedb9
Carlos Teixeira <carlosteixeira.email@gmail.com>
2020-12-09 20:25:13 +0000

    .

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $('#votes').on('click', () => {
     if($('.votes').is(":visible")) {
         $('.votes').fadeOut(500);
+        $('.holder').show(0);
     } else {
+        $('.holder').hide(0);
         $('.votes').fadeIn(500);
     }
 });
